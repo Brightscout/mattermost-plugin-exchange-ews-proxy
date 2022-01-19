30f360561490a000a112a7f943daeee800baa84f
shivamjosh <85667960+shivamjosh@users.noreply.github.com>
2022-01-19 19:21:31 +0530

    MI-1612: Importing configuration from application.properties file (#9)

--- src/main/java/com/ews/ews/service/impl/EWSServiceImpl.java
+++ src/main/java/com/ews/ews/service/impl/EWSServiceImpl.java
@@ -1,8 +1,8 @@
 package com.ews.ews.service.impl;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.ews.ews.service.EWSService;
@@ -10,7 +10,6 @@ import com.ews.ews.service.EWSService;
 import microsoft.exchange.webservices.data.core.ExchangeService;
 import microsoft.exchange.webservices.data.core.enumeration.misc.ConnectingIdType;
 import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
-import microsoft.exchange.webservices.data.core.enumeration.misc.TraceFlags;
 import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
 import microsoft.exchange.webservices.data.credential.WebCredentials;
 import microsoft.exchange.webservices.data.misc.ImpersonatedUserId;
@@ -20,20 +19,27 @@ public class EWSServiceImpl implements EWSService {
 	
 	private ExchangeService service;
 	
+	@Value("${username}")
+	String username;
+
+	@Value("${password}")
+	String password;
+
+	@Value("${domain}")
+	String domain;
+
+	@Value("exchangeServerURL")
+	String exchangeServerURL;
+
 	public EWSServiceImpl() {
 		System.out.println("Initialising EWS service with service account credentials");
 		this.service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
-		// TODO: Fetch credentials from environment variables
-		String userName = "ServiceAccount";
-		String password = "ubL.qJIuNv5kUNAgdmnL.6l@VKY%J*gQ";
-		String domain = "brightscout";
-		ExchangeCredentials credentials = new WebCredentials(userName, password, domain);
+
+		ExchangeCredentials credentials = new WebCredentials(username, password, domain);
 		this.service.setCredentials(credentials);
 		this.service.setTraceEnabled(true);
-		// TODO: Fetch exchange server URL from environment variables
-		String exchangeURL = "https://exchangenode1.ad.brightscout.com/ews/exchange.asmx";
 		try {
-			this.service.setUrl(new URI(exchangeURL));
+			this.service.setUrl(new URI(exchangeServerURL));
 		} catch (Exception e) {
 			System.out.println("Failed to discover URL");
 			e.printStackTrace();
