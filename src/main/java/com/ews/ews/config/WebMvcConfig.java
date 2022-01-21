0df376d378a7daab24a25bfc2082318582f485d4
Shivam Chauhan <shivam.chauhan@joshtechnologygroup.com>
2022-01-21 18:25:55 +0530

    MI-1616: Added spring security token authentication

--- src/main/java/com/ews/ews/config/WebMvcConfig.java
+++ src/main/java/com/ews/ews/config/WebMvcConfig.java
@@ -1,20 +1,28 @@
 package com.ews.ews.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.ews.ews.middleware.AuthMiddleware;
+import com.ews.ews.security.AuthEntryPoint;
+import com.ews.ews.security.AuthenticationFilter;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+@EnableWebSecurity
+public class WebMvcConfig extends WebSecurityConfigurerAdapter {
 
-	@Value("${app.secretAPIKey}")
-	private String secretAPIKey;
-	
+	@Autowired
+	AuthenticationFilter authenticationFilter;
+
 	@Override
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(new AuthMiddleware(this.secretAPIKey));
-    }
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().exceptionHandling().authenticationEntryPoint(new AuthEntryPoint()).and()
+				.authorizeRequests().anyRequest().authenticated().and().csrf().disable().formLogin().disable()
+				.httpBasic().disable().logout().disable();
+
+		http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+	}
 }
---
+++ src/main/java/com/ews/ews/security/AuthenticationFilter.java
@@ -0,0 +1,51 @@
+package com.ews.ews.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.ews.ews.exception.UnauthorizedException;
+import com.ews.ews.payload.ApiResponse;
+
+@Component
+public class AuthenticationFilter extends OncePerRequestFilter {
+	
+	@Value("${app.secretAuthKey}")
+	private String secretAuthKey;
+
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		try {
+			String token = getTokenFromRequest(request);
+			if (StringUtils.hasText(token) && secretAuthKey != null && secretAuthKey.equals(token)) {
+				UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(token, token, null);
+				SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+			}
+		} catch (Exception ex) {
+			throw new UnauthorizedException(new ApiResponse(Boolean.FALSE, "Could not set user authentication in security context. Error: %s"+ ex.getMessage()));
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	private String getTokenFromRequest(HttpServletRequest request) {
+		String bearerToken = request.getHeader("Authorization");
+		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7, bearerToken.length());
+		}
+		return null;
+	}
+
+}
