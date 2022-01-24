702ab55cdd3f8dcf97db90f72416f7da8f010777
shivamjosh <85667960+shivamjosh@users.noreply.github.com>
2022-01-24 13:32:57 +0530

    MI-1607: Added batch request event API (#5)
    * MI-1607: WIP
    
    * MI-1607: Done review fixes
    
    * MI-1609: Made changes in get events API to correct status sync functionality (#6)
    
    * MI-1609: WIP
    
    * MI-1609: Made changes in get events API to correct status sync functionality
    
    * MI-1609: Done review fixes
    
    * MI-1610: Added exception handling in api's (#8)
    
    * MI-1610: WIP
    
    * MI-1610: WIP
    
    * MI-1610: Added exception handling in api's
    
    * MI-1610: Done review fixes
    
    * MI-1610: Done review fixes

---
+++ src/main/java/com/ews/ews/service/impl/BatchServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ews.ews.service.impl;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.ews.ews.model.CalendarViewBatchRequest;
+import com.ews.ews.model.CalendarViewBatchResponse;
+import com.ews.ews.model.CalendarViewSingleRequest;
+import com.ews.ews.model.CalendarViewSingleResponse;
+import com.ews.ews.model.event.Event;
+import com.ews.ews.service.BatchService;
+import com.ews.ews.service.EWSService;
+import com.ews.ews.service.EventService;
+
+@Service
+public class BatchServiceImpl implements BatchService {
+
+	@Autowired
+	EventService eventService;
+
+	@Autowired
+	EWSService ewsService;
+
+	@Override
+	public ResponseEntity<CalendarViewBatchResponse> getEvents(CalendarViewBatchRequest requests) throws Exception {
+		ArrayList<CalendarViewSingleResponse> responses = new ArrayList<>();
+		for (CalendarViewSingleRequest request : requests.getRequests()) {
+			ResponseEntity<ArrayList<Event>> response = this.eventService.getEvents(
+					this.ewsService.impersonateUser(request.getId()), request.getStartDateTime(),
+					request.getEndDateTime());
+			responses.add(new CalendarViewSingleResponse(request.getId(), response.getBody()));
+		}
+
+		return new ResponseEntity<>(new CalendarViewBatchResponse(responses), HttpStatus.OK);
+	}
+
+}
