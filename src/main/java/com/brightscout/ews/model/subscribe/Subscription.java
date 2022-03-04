5d6fc98be113b2336e2b437b6c15f1fc2ad31e34
shivamjosh <85667960+shivamjosh@users.noreply.github.com>
2022-03-04 16:23:59 +0530

    MI-1687: Bug fixes (#25)

--- src/main/java/com/brightscout/ews/model/subscribe/Subscription.java
+++ src/main/java/com/brightscout/ews/model/subscribe/Subscription.java
@@ -1,12 +1,14 @@
 package com.brightscout.ews.model.subscribe;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @RequiredArgsConstructor
 public class Subscription {
 	
