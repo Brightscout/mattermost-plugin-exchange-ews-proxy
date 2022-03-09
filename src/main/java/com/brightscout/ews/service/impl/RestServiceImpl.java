5f35ef584b7d3454aae4284e87fe0a4ec7cfde32
Shivam Chauhan <shivam.chauhan@joshtechnologygroup.com>
2022-03-09 17:31:13 +0530

    MI-1691: Handle exception

--- src/main/java/com/brightscout/ews/service/impl/RestServiceImpl.java
+++ src/main/java/com/brightscout/ews/service/impl/RestServiceImpl.java
@@ -28,22 +28,33 @@ public class RestServiceImpl implements RestService {
 
 	@Override
 	public void syncSubscriptions() {
-		String url = String.format("%s%s%s%s", mattermostPluginBaseUrl, AppConstants.PATH_API, AppConstants.PATH_SYNC,
-				AppConstants.PATH_SUBSCRIPTION);
-		// send sync subscription request
-		restTemplate.getForEntity(url, Void.class);
+		try {
+			String url = String.format("%s%s%s%s", mattermostPluginBaseUrl, AppConstants.PATH_API, AppConstants.PATH_SYNC,
+					AppConstants.PATH_SUBSCRIPTION);
+			// send sync subscription request
+			restTemplate.getForEntity(url, Void.class);
+		} catch (Exception e) {
+			// TODO: Log the error
+			System.out.println("error occurred while syncing subscriptions. Error: "+ e.getMessage());
+		}
 	}
 
 	@Override
 	public boolean getSubscriptionByID(String subscriptionID) {
-		String url = String.format("%s%s%s/%s", mattermostPluginBaseUrl, AppConstants.PATH_API,
-				AppConstants.PATH_SUBSCRIPTION, AppUtils.encodeBase64String(subscriptionID));
-		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-		if (response.getStatusCode() != HttpStatus.OK) {
+		try {
+			String url = String.format("%s%s%s/%s", mattermostPluginBaseUrl, AppConstants.PATH_API,
+					AppConstants.PATH_SUBSCRIPTION, AppUtils.encodeBase64String(subscriptionID));
+			ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+			if (response.getStatusCode() != HttpStatus.OK) {
+				// TODO: Log the error
+				System.out.println("error occurred while fetching subscription by id.");
+				return false;
+			}
+			return Boolean.parseBoolean(response.getBody());
+		} catch (Exception e) {
 			// TODO: Log the error
-			System.out.println("error occurred while fetching subscription by id.");
+			System.out.println("error occurred while fetching subscription by id. Error: "+ e.getMessage());
 			return false;
 		}
-		return Boolean.parseBoolean(response.getBody());
 	}
 }
